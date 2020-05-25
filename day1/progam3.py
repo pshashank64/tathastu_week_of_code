798afec7a60693ad51b0a333adb4df2bfe2b92e6
Shashank <65887227+pshashank64@users.noreply.github.com>
2020-05-25 04:37:32 -0700

    Create progam3.py

---
+++ day1/progam3.py
@@ -0,0 +1,8 @@
+x = float(input("ENter first number: "))
+y = float(input("Enter the second number: "))
+x = x + y
+y = x - y
+x = x - y
+print("Swapping result: ")
+print("Number 1 is: ", x)
+print("Number 2 is: ", y)
